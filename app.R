0d9cd7e6bb13a264bf515774168446b2fb8cf3a1
Yu-Zhen Tu <corytu@users.noreply.github.com>
2017-08-23 22:04:44 +0800

    Update app.R

--- app.R
+++ app.R
@@ -16,7 +16,7 @@ ui <- fluidPage(
                    c("老年人口百分比", "高齡類型")),
       helpText(HTML("區域邊界資料來源：<a href=\"http://data.gov.tw/dataset/7441\">內政部國土測繪中心 [2017] 鄉鎮市區界線（TWD97經緯度）</a>")),
       helpText(HTML("老化人口資料來源：<a href=\"https://data.gov.tw/dataset/8411\">內政部戶政司 [2017] 各村（里）戶籍人口統計月報表</a>")),
-      helpText(HTML("此開放資料依<a href=\"https://data.gov.tw/license\">政府資料開放授權條款（Open Government Data License）</a>進行公眾釋出，使用者於遵守本條款各項規定之前提下，得利用之。"))
+      helpText(HTML("此開放資料依<a href=\"https://data.gov.tw/license\">政府資料開放授權條款（Open Government Data License）</a>進行公眾釋出，使用者於遵守本條款各項規定之前提下，得利用之。")),
       helpText("老化數據資料整理：張永泓"),
       helpText("系統建置暨維護：涂玉臻"),
       helpText("最後更新：106年8月")
