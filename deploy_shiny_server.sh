5190a831957d419806c7e957fa0350ae1273842e
Yu-Zhen Tu <corytu@users.noreply.github.com>
2018-05-12 19:18:44 +0800

    Specify shells and flag the VM zone

--- deploy_shiny_server.sh
+++ deploy_shiny_server.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/env bash
+
 yum install -y R libpng-static libpng-devel geos geos-devel wget
 su - -c "R -e \"install.packages(c('shiny', 'magrittr', 'maptools', 'leaflet', 'rgeos'), repo = 'https://cloud.r-project.org/')\""
 wget https://download3.rstudio.org/centos6.3/x86_64/shiny-server-1.5.7.907-rh6-x86_64.rpm
 yum install -y --nogpgcheck shiny-server-1.5.7.907-rh6-x86_64.rpm
-# gcloud compute firewall-rules create shiny-conn --allow=tcp:3838 --target-tags shiny-server  --quiet
-gcloud compute instances add-tags instance-1 --tags shiny-server --quiet
+# gcloud compute firewall-rules create shiny-conn --allow=tcp:3838 --target-tags shiny-server
+gcloud compute instances add-tags instance-1 --tags shiny-server --zone asia-east1-b
 # git clone from https://github.com/corytu/OldPopulationProportions.git earlier
 mv ./OldPopulationProportions/ /srv/shiny-server/
